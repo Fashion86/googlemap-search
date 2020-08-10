c650de73c09f5d9a6f2ff3d1ef4398aedf781614
GoldMiner <xiaoyan8622@gmail.com>
2020-08-10 15:18:48 +0900

    8.10 --3

--- front/src/app/components/home/home.component.ts
+++ front/src/app/components/home/home.component.ts
@@ -17,6 +17,8 @@ export class HomeComponent implements OnInit {
   @ViewChild('map') mapElement: any;
   map: google.maps.Map;
   drawingManager: any;
+  drawpol = true;
+  markers: any[] = [];
   constructor(private http: HttpClient, private _formBuilder: FormBuilder, private _productService: ProductService) {
   }
 
@@ -42,13 +44,14 @@ export class HomeComponent implements OnInit {
     //     drawingModes: [
     //       // google.maps.drawing.OverlayType.MARKER,
     //       // google.maps.drawing.OverlayType.CIRCLE,
-    //       google.maps.drawing.OverlayType.POLYGON,
+    //       // google.maps.drawing.OverlayType.POLYGON,
     //       // google.maps.drawing.OverlayType.POLYLINE,
     //       // google.maps.drawing.OverlayType.RECTANGLE
     //     ]
     //   },
     //   polygonOptions: {
-    //     clickable:  false
+    //     clickable:  false,
+    //     draggable: true
     //   }
     // });
     // this.drawingManager.setMap(this.map);
@@ -58,54 +61,78 @@ export class HomeComponent implements OnInit {
 
     // @ts-ignore TODO(jpoehnelt)
     centerControlDiv.index = 1;
-
     this.map.controls[google.maps.ControlPosition.TOP_RIGHT].push(centerControlDiv);
+
+    let poly = new google.maps.Polyline({
+      map: this.map,
+      clickable: true
+    });
+    const move = google.maps.event.addListener(this.map, 'click', (e) => {
+      poly.getPath().push(e.latLng);
+    });
+
+    google.maps.event.addListenerOnce(this.map, 'mouseup', (e) => {
+      google.maps.event.removeListener(move);
+      if (this.drawpol) {
+        const path = poly.getPath();
+        poly.setMap(null);
+
+        poly = new google.maps.Polygon({
+          map: this.map,
+          paths: path
+        });
+      }
+    });
   }
 
   getPropertyList(): void {
     this._productService.getPropertyList(null).subscribe(res => {
       if (res['success']) {
         this.datas = res['data'];
-        this.datas.forEach(d => {
-          const marker = new google.maps.Marker({
-            position: new google.maps.LatLng(Number(parseFloat(d.latitude).toFixed(10)), Number(parseFloat(d.longitude).toFixed(10))),
-            map: this.map,
-            icon: {
-              path: google.maps.SymbolPath.CIRCLE,
-              scale: 5
-            }
-          });
-
-          google.maps.event.addListener(marker, 'click', () => {
-            this.map.setZoom(12);
-            this.map.setCenter(marker.getPosition());
-          });
-          const contentString = '<div class="marker-content">' +
-            '</div>' +
-            '<h7 class="firstHeading"><strong>$' + Math.round(d.listprice / 1000) + 'K</strong></h7>' +
-            '</div>';
-
-          const infowindow = new google.maps.InfoWindow({
-            content: contentString
-          });
-          // let pp = document.getElementsByClassName('gm-style-iw-c');
-          infowindow.open(this.map, marker);
-          google.maps.event.addDomListener(infowindow, 'domready', () => {
-            $('.firstHeading').click(() => {
-              console.log("Hello World");
+        for (let i = 0; i <= this.datas.length; i++) {
+          if (this.datas[i]) {
+            this.markers[i] = new google.maps.Marker({
+              position: new google.maps.LatLng(Number(parseFloat(this.datas[i].latitude).toFixed(10)), Number(parseFloat(this.datas[i].longitude).toFixed(10))),
+              map: this.map,
+              icon: {
+                path: google.maps.SymbolPath.CIRCLE,
+                scale: 0
+              }
             });
-          });
-          google.maps.event.addListener(infowindow, 'domready', function() {
 
-          });
-          // google.maps.event.addListener(marker, 'mouseout', () => {
-          //   infowindow.close();
-          // });
-        });
+            // google.maps.event.addListener(this.markers[i], 'click', () => {
+            //   this.map.setZoom(12);
+            //   this.map.setCenter(this.markers[i].getPosition());
+            // });
+            const contentString = '<div class="marker-content">' +
+              '</div>' +
+              '<h7 id="firstHeading' + i + '"><strong class="marker-text">$' + Math.round(this.datas[i].listprice / 1000) + 'K</strong></h7>' +
+              '</div>';
+
+            const infowindow = new google.maps.InfoWindow({
+              content: contentString
+            });
+            // let pp = document.getElementsByClassName('gm-style-iw-c');
+            infowindow.open(this.map, this.markers[i]);
+            google.maps.event.addDomListener(infowindow, 'domready', () => {
+              const idstring = '#firstHeading' + i;
+              $('#firstHeading' + i).click(() => {
+                console.log("Hello World" + this.datas[i].id);
+              });
+            });
+          }
+        }
       }
     });
   }
 
+  polyAction() {
+    if (this.drawpol) {
+
+    } else {
+
+    }
+  }
   CenterControl(controlDiv: Element, map: google.maps.Map) {
     // Set CSS for the control border.
     const controlUI = document.createElement("div");
@@ -122,19 +149,32 @@ export class HomeComponent implements OnInit {
     controlDiv.appendChild(controlUI);
 
     // Set CSS for the control interior.
-    const controlText = document.createElement("div");
+    let controlText = document.createElement("div");
     controlText.style.color = "rgb(25,25,25)";
     controlText.style.fontFamily = "Roboto,Arial,sans-serif";
     controlText.style.fontSize = "16px";
     controlText.style.lineHeight = "38px";
     controlText.style.paddingLeft = "5px";
     controlText.style.paddingRight = "5px";
-    controlText.innerHTML = "Cen";
+    controlText.style.width = "35px";
+    controlText.style.height = "35px";
+    controlText.innerHTML = "<svg viewBox=\"0 0 24 24\" class=\"cy-map-button-polygon-open sc-bdVaJa bssMCl\">" +
+      "<path d=\"M20,17.6l2-11c1.1-0.1,2-1,2-2.1c0-1.2-1-2.1-2.2-2.1c-1.1,0-1.9,0.7-2.1,1.7L4.2,5.7C3.8,5,3.1,4.5,2.2,4.5 C1,4.5,0,5.5,0,6.7c0,1.2,1,2.1,2.2,2.1c0.1,0,0.2,0,0.3,0l2.6,5.9c-0.5,0.4-0.8,1-0.8,1.6c0,1.2,1,2.1,2.2,2.1c0.8,0,1.5-0.4,1.9-1 l8.4,2.2c0.1,1.2,1,2.1,2.2,2.1c1.2,0,2.2-1,2.2-2.2C21.2,18.7,20.7,17.9,20,17.6z M3.6,8.3C4,7.9,4.3,7.5,4.3,6.9l15.5-1.7 c0.2,0.5,0.5,0.9,1,1.1l-2,11c-0.7,0.1-1.4,0.5-1.7,1.1l-8.4-2.2c0-1.2-1-2.1-2.2-2.1c-0.1,0-0.2,0-0.3,0L3.6,8.3z\">" +
+      "</path></svg>";
     controlUI.appendChild(controlText);
 
     // Setup the click event listeners: simply set the map to Chicago.
     controlUI.addEventListener("click", () => {
-      this.map.setZoom(15);
+      this.drawpol = !this.drawpol;
+      if (this.drawpol) {
+        controlText.innerHTML = "<svg viewBox=\"0 0 24 24\" class=\"cy-map-button-polygon-open sc-bdVaJa bssMCl\">" +
+          "<path d=\"M20,17.6l2-11c1.1-0.1,2-1,2-2.1c0-1.2-1-2.1-2.2-2.1c-1.1,0-1.9,0.7-2.1,1.7L4.2,5.7C3.8,5,3.1,4.5,2.2,4.5 C1,4.5,0,5.5,0,6.7c0,1.2,1,2.1,2.2,2.1c0.1,0,0.2,0,0.3,0l2.6,5.9c-0.5,0.4-0.8,1-0.8,1.6c0,1.2,1,2.1,2.2,2.1c0.8,0,1.5-0.4,1.9-1 l8.4,2.2c0.1,1.2,1,2.1,2.2,2.1c1.2,0,2.2-1,2.2-2.2C21.2,18.7,20.7,17.9,20,17.6z M3.6,8.3C4,7.9,4.3,7.5,4.3,6.9l15.5-1.7 c0.2,0.5,0.5,0.9,1,1.1l-2,11c-0.7,0.1-1.4,0.5-1.7,1.1l-8.4-2.2c0-1.2-1-2.1-2.2-2.1c-0.1,0-0.2,0-0.3,0L3.6,8.3z\">" +
+          "</path></svg>";
+      } else {
+        controlText.innerHTML = "<svg viewBox=\"0 0 24 24\" class=\"cy-map-button-polygon-close sc-bdVaJa bssMCl\"><path d=\"M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z\">" +
+          "</path></svg>";
+      }
+      this.polyAction();
     });
   }
 
