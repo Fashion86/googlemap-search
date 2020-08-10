3042aa241d50df6456a492c4348b848a9b87f83a
Hong 86 <msjkgoodluck@gmail.com>
2020-08-10 08:32:32 +0800

    commit 8/10 --1

--- front/src/app/components/home/home.component.ts
+++ front/src/app/components/home/home.component.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {} from 'googlemaps';
 import {ProductService} from '../../services/product.service';
+declare var $: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -20,9 +21,8 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.getPropertyList();
     const mapProperties = {
-      center: new google.maps.LatLng(this.lat, this.lng),
+      center: new google.maps.LatLng(this.lat,  this.lng),
       zoom: 10,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
       mapTypeControl: false,
@@ -33,7 +33,7 @@ export class HomeComponent implements OnInit {
       },
     };
     this.map = new google.maps.Map(this.mapElement.nativeElement,    mapProperties);
-
+    this.getPropertyList();
   }
 
   getPropertyList(): void {
@@ -43,30 +43,116 @@ export class HomeComponent implements OnInit {
         this.datas.forEach(d => {
           const marker = new google.maps.Marker({
             position: new google.maps.LatLng(Number(parseFloat(d.latitude).toFixed(10)), Number(parseFloat(d.longitude).toFixed(10))),
-            map: this.map
+            map: this.map,
+            icon: {
+              path: google.maps.SymbolPath.CIRCLE,
+              scale: 5
+            }
           });
+
           google.maps.event.addListener(marker, 'click', () => {
             this.map.setZoom(12);
             this.map.setCenter(marker.getPosition());
           });
-          const contentString = '<div id="content">' +
-            '<div id="siteNotice">' +
+          const contentString = '<div class="marker-content">' +
             '</div>' +
-            '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-            '</div>' +
+            '<h7 class="firstHeading"><strong>$' + Math.round(d.listprice / 1000) + 'K</strong></h7>' +
             '</div>';
 
           const infowindow = new google.maps.InfoWindow({
             content: contentString
           });
-          google.maps.event.addListener(marker, 'mouseover', () => {
-            infowindow.open(this.map, marker);
+          // let pp = document.getElementsByClassName('gm-style-iw-c');
+          infowindow.open(this.map, marker);
+          google.maps.event.addDomListener(infowindow, 'domready', () => {
+            // $('.firstHeading').click(() => {
+            //   console.log("Hello World");
+            // });
           });
-          google.maps.event.addListener(marker, 'mouseout', () => {
-            infowindow.close();
+          google.maps.event.addListener(infowindow, 'domready', function() {
+
           });
+          // google.maps.event.addListener(marker, 'mouseout', () => {
+          //   infowindow.close();
+          // });
         });
       }
     });
+  }
+
+
+}
+
+class USGSOverlay extends google.maps.OverlayView {
+  private bounds_: google.maps.LatLngBounds;
+  private image_: string;
+  private div_: HTMLElement | null;
+
+  constructor(bounds: google.maps.LatLngBounds, image: string) {
+    super();
+
+    // Initialize all properties.
+    this.bounds_ = bounds;
+    this.image_ = image;
+
+    // Define a property to hold the image's div. We'll
+    // actually create this div upon receipt of the onAdd()
+    // method so we'll leave it null for now.
+    this.div_ = null;
+  }
+
+  /**
+   * onAdd is called when the map's panes are ready and the overlay has been
+   * added to the map.
+   */
+  onAdd() {
+    this.div_ = document.createElement("div");
+    this.div_.style.borderStyle = "none";
+    this.div_.style.borderWidth = "0px";
+    this.div_.style.position = "absolute";
+
+    // Create the img element and attach it to the div.
+    const img = document.createElement("div");
+    img.style.cssText = "border: 1px solid black; margin-top: 8px; background: yellow; padding: 5px;";
+    img.innerHTML = "City Hall, Sechelt<br>British Columbia<br>Canada";
+    this.div_.appendChild(img);
+
+    // Add the element to the "overlayLayer" pane.
+    const panes = this.getPanes();
+    panes.overlayLayer.appendChild(this.div_);
+  }
+
+  draw() {
+    // We use the south-west and north-east
+    // coordinates of the overlay to peg it to the correct position and size.
+    // To do this, we need to retrieve the projection from the overlay.
+    const overlayProjection = this.getProjection();
+
+    // Retrieve the south-west and north-east coordinates of this overlay
+    // in LatLngs and convert them to pixel coordinates.
+    // We'll use these coordinates to resize the div.
+    const sw = overlayProjection.fromLatLngToDivPixel(
+      this.bounds_.getSouthWest()
+    );
+    const ne = overlayProjection.fromLatLngToDivPixel(
+      this.bounds_.getNorthEast()
+    );
+
+    // Resize the image's div to fit the indicated dimensions.
+    if (this.div_) {
+      this.div_.style.left = sw.x + "px";
+      this.div_.style.top = ne.y + "px";
+      this.div_.style.width = ne.x - sw.x + "px";
+      this.div_.style.height = sw.y - ne.y + "px";
+    }
+  }
+
+  // The onRemove() method will be called automatically from the API if
+  // we ever set the overlay's map property to 'null'.
+  onRemove() {
+    if (this.div_) {
+      (this.div_.parentNode as HTMLElement).removeChild(this.div_);
+      this.div_ = null;
+    }
   }
 }
